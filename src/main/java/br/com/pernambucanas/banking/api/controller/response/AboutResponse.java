082b4c04b0f75f8ed2361453802562f60a340449
Pedro Henrique Castro <pedro.castro@luizalabs.com>
2024-07-24 11:25:40 -0300

    test sonar new build

--- src/main/java/br/com/pernambucanas/banking/api/controller/response/AboutResponse.java
+++ src/main/java/br/com/pernambucanas/banking/api/controller/response/AboutResponse.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class AboutResponse {
 
-    private String version;
+    private final String version;
 
     public AboutResponse() {
         this.version = getAppVersion();
@@ -17,19 +17,19 @@ public class AboutResponse {
     }
 
     private String getAppVersion() {
-        String version = "UNKNOWN";
+        String appVersion = "UNKNOWN";
 
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(
                 "META-INF/maven/br.com.pernambucanas.banking/api/pom.properties")) {
             if (inputStream != null) {
                 Properties properties = new Properties();
                 properties.load(inputStream);
-                version = properties.getProperty("version", "UNKNOWN");
+                appVersion = properties.getProperty("version", "UNKNOWN");
             }
         } catch (IOException e) {
             // handle exceptions here
         }
 
-        return version;
+        return appVersion;
     }
 }
